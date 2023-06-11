97b3db89005e99994204a3f1f215c2ff8a1d8404
Alba <alba_martina@hotmail.com>
2023-06-11 14:12:17 -0300

    cambios 11/06

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 window.addEventListener('load', () => {
 
 
-    // initial slide
+    // diapositiva inicial
     let slide = 1;
 
-    // total slides
+    // total diapositivas
     let slides = document.querySelectorAll(".slider ul li");
     total = slides.length;
 
-    // show first side
+    // mostrar
     showSlide(1);
 
     next = document.querySelector(".next");
     prev = document.querySelector(".prev")
 
-    /**
-     * event next button
-     */
+    
+    // boton siguiente
+     
     next.addEventListener('click', (evt) => {
         evt.preventDefault();
         slide++;
@@ -24,9 +24,9 @@ window.addEventListener('load', () => {
         showSlide(slide);
     })
 
-    /** 
-     * event prev button
-     */
+    
+    //   boton anterior
+     
     prev.addEventListener("click", (evt) => {
         evt.preventDefault();
         slide--;
@@ -35,14 +35,14 @@ window.addEventListener('load', () => {
     })
 
     /**
-     * show slides
+     * mostrar deslizador
      * 
      * @param {number} n 
      * @return {null}
      * 
      */
     function showSlide(n) {
-        n--; // decrement 1
+        n--; // decrementa 1
         for (i = 0; i < slides.length; i++) {
             (i == n) ? slides[n].style.display = "block" : slides[i].style.display = "none";
         }
@@ -59,4 +59,43 @@ function onClick(element) {
 function redirect()
 {
 window.location.href="https://api.whatsapp.com/send?phone=543493461833";
+}
+// form
+var myform = document.getElementById("FormId");
+document.getElementById("LinkId").addEventListener("click", function () {
+  myform.submit();
+});
+
+// button card principal
+function irAlSegundoDiv() {
+    // Obtener el documento del segundo HTML
+    var iframe = document.createElement('iframe');
+    iframe.src = 'adopcion.html';
+    iframe.style.display = 'none';
+
+    // Añadir el iframe al cuerpo del documento actual
+    document.body.appendChild(iframe);
+
+    // Obtener el segundo div dentro del iframe
+    iframe.onload = function() {
+      var segundoDiv = iframe.contentDocument.getElementById('div-a-unir');
+      
+      // Desplazarse al segundo div
+      segundoDiv.scrollIntoView({ behavior: 'smooth' });
+
+      // Eliminar el iframe
+      setTimeout(function() {
+        document.body.removeChild(iframe);
+      }, 1000); // Puedes ajustar este tiempo según tus necesidades
+    };
+  }
+
+// slider rescates
+
+
+// toggle
+function mobile(){
+  var element = document.getElementById("idMobile");
+	element.classList.toggle("mobile");
+
 }
