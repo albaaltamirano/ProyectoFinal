f617319d2cbc238bd85b674a497ee6e5e027c085
Alba <alba_martina@hotmail.com>
2023-06-29 20:59:39 -0300

    29/06

--- script.js
+++ script.js
@@ -1,64 +1,63 @@
 window.addEventListener('load', () => {
 
 
-    // diapositiva inicial
-    let slide = 1;
+  // diapositiva inicial
+  let slide = 1;
 
-    // total diapositivas
-    let slides = document.querySelectorAll(".slider ul li");
-    total = slides.length;
+  // total diapositivas
+  let slides = document.querySelectorAll(".slider ul li");
+  total = slides.length;
 
-    // mostrar
-    showSlide(1);
+  // mostrar
+  showSlide(1);
 
-    next = document.querySelector(".next");
-    prev = document.querySelector(".prev")
+  next = document.querySelector(".next");
+  prev = document.querySelector(".prev")
 
-    
-    // boton siguiente
-     
-    next.addEventListener('click', (evt) => {
-        evt.preventDefault();
-        slide++;
-        if (slide > total) { slide = 1; }
-        showSlide(slide);
-    })
 
-    
-    //   boton anterior
-     
-    prev.addEventListener("click", (evt) => {
-        evt.preventDefault();
-        slide--;
-        if (slide < 1) { slide = total; }
-        showSlide(slide);
-    })
+  // boton siguiente
 
-    /**
-     * mostrar deslizador
-     * 
-     * @param {number} n 
-     * @return {null}
-     * 
-     */
-    function showSlide(n) {
-        n--; // decrementa 1
-        for (i = 0; i < slides.length; i++) {
-            (i == n) ? slides[n].style.display = "block" : slides[i].style.display = "none";
-        }
+  next.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    slide++;
+    if (slide > total) { slide = 1; }
+    showSlide(slide);
+  })
+
+
+  //   boton anterior
+
+  prev.addEventListener("click", (evt) => {
+    evt.preventDefault();
+    slide--;
+    if (slide < 1) { slide = total; }
+    showSlide(slide);
+  })
+
+  /**
+   * mostrar deslizador
+   * 
+   * @param {number} n 
+   * @return {null}
+   * 
+   */
+  function showSlide(n) {
+    n--; // decrementa 1
+    for (i = 0; i < slides.length; i++) {
+      (i == n) ? slides[n].style.display = "block" : slides[i].style.display = "none";
     }
+  }
 
 });
 
 //modal
 function onClick(element) {
-    document.getElementById("img01").src = element.src;
-    document.getElementById("modal01").style.display = "block";
-  }
+  document.getElementById("img01").src = element.src;
+  document.getElementById("modal01").style.display = "block";
+}
 // boton-adopción
-function redirect()
-{
-window.location.href="https://api.whatsapp.com/send?phone=543493461833";
+function redirect() {
+  window.location.href = "https://api.whatsapp.com/send?phone=543493461833";
 }
 // form
 var myform = document.getElementById("FormId");
@@ -66,36 +65,52 @@ document.getElementById("LinkId").addEventListener("click", function () {
   myform.submit();
 });
 
-// button card principal
-function irAlSegundoDiv() {
-    // Obtener el documento del segundo HTML
-    var iframe = document.createElement('iframe');
-    iframe.src = 'adopcion.html';
-    iframe.style.display = 'none';
-
-    // Añadir el iframe al cuerpo del documento actual
-    document.body.appendChild(iframe);
-
-    // Obtener el segundo div dentro del iframe
-    iframe.onload = function() {
-      var segundoDiv = iframe.contentDocument.getElementById('div-a-unir');
-      
-      // Desplazarse al segundo div
-      segundoDiv.scrollIntoView({ behavior: 'smooth' });
-
-      // Eliminar el iframe
-      setTimeout(function() {
-        document.body.removeChild(iframe);
-      }, 1000); // Puedes ajustar este tiempo según tus necesidades
-    };
-  }
-
-// slider rescates
-
 
 // toggle
-function mobile(){
+function mobile() {
   var element = document.getElementById("idMobile");
-	element.classList.toggle("mobile");
-
+  element.classList.toggle("mobile");
+  let overlayDiv = document.querySelector(".overlay");
+  console.log(overlayDiv.style.opacity);
+  if (overlayDiv.style.opacity === '0')
+    overlayDiv.style.opacity = 1;
+  else
+    overlayDiv.style.opacity = 0;
+  
 }
+
+
+//overlay
+// $(".toggle").on('click',function(){
+//   $(this).children(".overlay").toggleClass("show");
+// });
+
+Array.from(document.getElementsByClassName("hover")).forEach(e => e.addEventListener("mouseleave", 
+  function () {
+    e.removeClass("hover");
+  }
+))
+
+// tabla castraciones
+var table = document.getElementById("racetimes");
+  var rows = table.getElementsByTagName("tr");
+
+  // Ocultar filas en pantallas pequeñas
+  if (window.innerWidth < 600) {
+    for (var i = 0; i < rows.length; i++) {
+      rows[i].style.display = "none";
+    }
+  }
+
+  // Agregar evento de redimensionamiento
+  window.addEventListener("resize", function() {
+    if (window.innerWidth < 600) {
+      for (var i = 0; i < rows.length; i++) {
+        rows[i].style.display = "none";
+      }
+    } else {
+      for (var i = 0; i < rows.length; i++) {
+rows[i].style.display = "table-row";
+}
+}
+});
